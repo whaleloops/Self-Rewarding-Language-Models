a2076be71b93efc1cbba088db22c810584c0348d
whaleloops <yzc315@qq.com>
2024-05-17 13:24:06 +0000

    added gitignore

--- scripts/run.sh
+++ scripts/run.sh
@@ -18,19 +18,23 @@ export CODE=/home/zhichaoyang/Self-Rewarding-Language-Models/scripts
 export MODEL_NAME=M0
 export DATA_DIR=/data/data_user_alpha/public_data/self_reward
 
+# M0
+CUDA_VISIBLE_DEVICES=7 python $CODE/02_gen_responses.py $TOKENIZER_MODEL $BASE_MODEL $DATA_DIR/$MODEL_NAME/generated/prompts_debug.jsonl $DATA_DIR/$MODEL_NAME/generated/responses_original.jsonl
 
-
-# Train the instruction following and evaluation skills
+# M1: Train the instruction following and evaluation skills
 CUDA_VISIBLE_DEVICES=3 python $CODE/00_sft.py -d $DATA_DIR/$MODEL_NAME/train/ift_eft.jsonl -b $TOKENIZER_MODEL -m $BASE_MODEL -o $DATA_DIR/$MODEL_NAME/models/sft
+CUDA_VISIBLE_DEVICES=3 python $CODE/02_gen_responses.py $TOKENIZER_MODEL $DATA_DIR/$MODEL_NAME/models/sft/final_checkpoint $DATA_DIR/$MODEL_NAME/generated/prompts_debug.jsonl $DATA_DIR/$MODEL_NAME/generated/responses.jsonl
 
 # Generate responses for the prompts
-CUDA_VISIBLE_DEVICES=3 python $CODE/02_gen_responses.py $DATA_DIR/$MODEL_NAME/models/sft/final_checkpoint $DATA_DIR/$MODEL_NAME/generated/prompts_debug.jsonl $DATA_DIR/$MODEL_NAME/generated/responses.jsonl
-
-# Generate responses for the prompts
-CUDA_VISIBLE_DEVICES=2 python $CODE/03_gen_scores.py $TOKENIZER_MODEL $DATA_DIR/$MODEL_NAME/models/sft/final_checkpoint $DATA_DIR/$MODEL_NAME/generated/responses.jsonl $DATA_DIR/$MODEL_NAME/generated/scores.jsonl
+CUDA_VISIBLE_DEVICES=3 python $CODE/03_gen_scores.py $TOKENIZER_MODEL $DATA_DIR/$MODEL_NAME/models/sft/final_checkpoint $DATA_DIR/$MODEL_NAME/generated/responses.jsonl $DATA_DIR/$MODEL_NAME/generated/scores.jsonl
 
 # Create DPO data
-CUDA_VISIBLE_DEVICES=2 python $CODE/04_gen_preferences.py $DATA_DIR/$MODEL_NAME/generated/scores.jsonl $DATA_DIR/$MODEL_NAME/generated/preferences.jsonl
+CUDA_VISIBLE_DEVICES=3 python $CODE/04_gen_preferences.py $DATA_DIR/$MODEL_NAME/generated/scores.jsonl $DATA_DIR/$MODEL_NAME/generated/preferences.jsonl
 
-# Train DPO model
-CUDA_VISIBLE_DEVICES=2 python $CODE/05_dpo.py $TOKENIZER_MODEL $DATA_DIR/$MODEL_NAME/models/sft/final_checkpoint $DATA_DIR/$MODEL_NAME/generated/preferences.jsonl $DATA_DIR/$MODEL_NAME/models/dpo/
+# M2: Train DPO model
+CUDA_VISIBLE_DEVICES=3 python $CODE/05_dpo.py $TOKENIZER_MODEL $DATA_DIR/$MODEL_NAME/models/sft/final_checkpoint $DATA_DIR/$MODEL_NAME/generated/preferences.jsonl $DATA_DIR/$MODEL_NAME/models/dpo/
+CUDA_VISIBLE_DEVICES=3 python $CODE/02_gen_responses.py $TOKENIZER_MODEL $DATA_DIR/$MODEL_NAME/models/dpo/final_checkpoint $DATA_DIR/$MODEL_NAME/generated/prompts_debug.jsonl $DATA_DIR/$MODEL_NAME/generated/responses_m2.jsonl
+
+
+
+
